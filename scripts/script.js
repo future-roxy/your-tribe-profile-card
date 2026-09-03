b5547f9f260cc47a0138a3251c7717684b73f589
Melissa <melissa.blankenburgh@hva.nl>
2026-09-03 10:56:00 +0200

    js annotaties toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
+// laat het programma weten welke DOM elementen we precies willen
 let ul = document.getElementById("toybox");
 let lis = ul.getElementsByTagName("li");
 
+// geef een random headerclass aan ieder element
 function assignHeader(item){
+  // random nummer tussen 0 en 2
   const number = Math.floor(Math.random() * 3);
 
+  // geef de class op basis van dat nummer
   switch(number){
     case 0:
       item.classList.add("topHeader");
@@ -21,6 +25,7 @@ function assignHeader(item){
   }
 }
 
+// geef een random header aan elke li die bovenaan de pagina is gevonden
 for (let i = 0; i < lis.length; ++i) {
   assignHeader(lis[i]);
 }
